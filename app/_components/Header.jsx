3ab74d05d14a24b9193bb1f0ef016fef53b40c9a
hengtan <hengjt1@gmail.com>
2024-05-22 12:26:23 -0300

    add components

---
+++ app/_components/Header.jsx
@@ -0,0 +1,80 @@
+"use client";
+import {Button} from "@/components/ui/button";
+import {LayoutGrid, Search, ShoppingBag} from "lucide-react";
+import Image from "next/image";
+import React, {useContext, useEffect, useState} from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import GlobalApi from "@/app/_utils/GlobalApi";
+
+function Header() {
+
+    const [categoryList, setCategoryList] = useState([]);
+
+    useEffect(() => {
+        getCategoryList();
+    }, []);
+
+    /*
+    GetCategoryList Data
+    */
+    const getCategoryList = () => {
+        GlobalApi.getCategory().then(res => {
+            setCategoryList(res.data.data)
+        })
+    }
+
+    return (
+        <div className="p-5 shadow-sm flex justify-between">
+            <div className="flex items-center gap-8">
+                <Image src="/logo.png" alt="logo" width={150} height={100}/>
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <h2
+                            className="hidden md:flex gap-2 items-center
+            border rounded-full p-2 px-10 bg-slate-200 cursor-pointer"
+                        >
+                            <LayoutGrid className="h-5 w-5"/> Category
+                        </h2>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+                        <DropdownMenuSeparator/>
+                        {categoryList.map((category, index) => (
+                            <DropdownMenuItem className="flex gap-2 items-center cursor-pointer">
+                                {/*<Image src={
+                                    process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+                                    category?.attributes?.icon.data.attributes?.url}
+                                       unoptimized={true}
+                                       alt='icon'
+                                       width={23}
+                                       height={23}
+                                />*/}
+                                <h2>{category?.attributes?.name}</h2>
+                            </DropdownMenuItem>
+                        ))}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <div className="md:flex gap-3 items-center border rounded-full p-2 px-5 hidden">
+                    <Search/>
+                    <input type="text" placeholder="Search" className="outline-none"/>
+                </div>
+            </div>
+            <div className="flex gap-5 items-center">
+                <h2 className="flex gap-2 items-center text-lg">
+                    <ShoppingBag/> 0{" "}
+                </h2>
+                <Button>Login</Button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ app/_utils/GlobalApi.jsx
@@ -0,0 +1,10 @@
+const {default: axios} = require("axios");
+
+const axiosClient = axios.create({
+    baseURL: 'http://localhost:1337/api',
+})
+
+const getCategory = () => axiosClient.get('/categories');
+/*Show with images
+const getCategory = () => axiosClient.get('/categories?populate=*');*/
+export default {getCategory};
